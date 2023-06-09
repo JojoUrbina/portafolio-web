72fc3b9fee2df2624621c11a76f815d0c96c4dd5
Jorman Urbina <jormanurbina1@gmail.com>
2023-06-09 02:01:42 -0300

    Descripción de los cambios en el código:
    Se agregó un script para cargar la biblioteca de emailjs.
    Se inicializó emailjs con una clave de API.
    Se corrigió la etiqueta de título del documento HTML.
    Se modificaron los enlaces del menú de navegación para que sean más legibles.
    Se añadieron las imágenes de las banderas para cambiar el idioma.
    Se ajustó la estructura y diseño de las secciones "Sobre Mi" y "Habilidades".
    Se agregó información adicional en la sección "Habilidades" sobre el uso de tecnologías como Bootstrap, TailwindCss, Sass y React.
    Se actualizó la descripción en la sección "Sobre Mi" para resaltar la experiencia en desarrollo de aplicaciones web.
    Se corrigió un error en el enlace hacia el repositorio de GitHub.
    Se realizaron otros cambios menores de estilo y organización del código.

--- script.js
+++ script.js
@@ -88,3 +88,26 @@ async function changeLanguage(language) {
     textToChange.placeholder = texts[section][value];
   }
 }
+
+//emailJS
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_6qjnkdj';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
